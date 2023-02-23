cc1f4c844f26c94004049a390d74de9e40ec05a2
Diego-PS <diegopsilvavrb@gmail.com>
2023-02-23 17:30:19 -0300

    Upload de arquivo

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
+const http = require('http');
+const express = require('express');
+const busboy = require('busboy');
+const path = require('path');
 
 var fs = require("fs");
-var http = require("http");
 var url = require("url");
 
-http.createServer(function (request, response) {
+const server = http.createServer(function (request, response) {
 
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
@@ -24,9 +25,27 @@ http.createServer(function (request, response) {
     } else if (pathname == "/style.css") {
         css = fs.readFileSync("style.css", "utf8");
         response.write(css);
+    } else if (pathname == "/file") {
+        html = fs.readFileSync("lerArquivos.html", "utf8");
+        response.write(html);
+    } else if (request.url === "/upload") {
+        let filename = '';
+        const bb = busboy({ headers: request.headers });
+        bb.on('file', (name, file, info) => {
+            filename = info.filename;
+            const saveTo = path.join(__dirname, '/uploads/' + filename);
+            file.pipe(fs.createWriteStream(saveTo));
+        });
+        bb.on('close', () => {
+            response.writeHead(200, { 'Content-Type': 'text/plain' });
+            response.write(`upload success: ${filename}`);
+        });
+        request.pipe(bb);
     }
 
     response.end();
-}).listen(8888);
+})
 
-console.log("Listening to server on 8888...");
+server.listen(8888, () => {
+    console.log('Server listening on http://localhost:8888 ...');
+});
