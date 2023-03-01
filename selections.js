5ee835741f6dc978e4b52bcad1329a98c15e1f46
Caio Jorge Carvalho Lara <2022043744>
2023-03-01 15:18:13 -0300

    nome_json

--- selections.js
+++ selections.js
@@ -350,7 +350,9 @@ function segmentar () {
   const link = document.createElement("a");
   const file = new Blob([JSON.stringify(json, null, 2)], { type: 'application/json' });
   link.href = URL.createObjectURL(file);
-  link.download = "sample.json";
+  var tamanho_nome = pdfComplexo.length;
+  var nome_json = pdfComplexo.substring(0, tamanho_nome - 4);
+  link.download = nome_json + ".json";
   link.click();
   URL.revokeObjectURL(link.href);
 
